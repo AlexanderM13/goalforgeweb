bfd6234fcfd80ad2e3e2fbdb3171957e85de9f91
AlexanderM13 <alexlosangeles7@gmail.com>
2025-06-15 19:53:04 -0700

    Delete src directory

--- src/main/java/project/goalforge/Utility/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package project.goalforge.Utility;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
